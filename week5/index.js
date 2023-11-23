00fda19734498ab2e78903d0764068952ddf758a
behlamustansir124 <bp2027@hw.ac.uk>
2023-11-23 17:16:33 +0400

    lab5 2.0

--- week5/index.js
+++ week5/index.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config({ path: 'C:\\Users\\User\\Documents\\GitHub\\F28WPlab1behla\\week5\\.env' });
+
 const app = express();
-const port=5000;
+const port = 5000;
+
+
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+db.connect((error) => {
+    if (error) {
+        console.error('Error connecting to MySQL:', error);
+    } else {
+        console.log('Connected to MySQL');
+    }
+});
+
 app.get('/', (req, res) => {
     res.send("<h1> home page </h1>");
+});
 
+app.use('/', require('./routes/pages'));
 
-    app.listen(port,()=>{
-        console.log(`server started on port ${port}.` );
-
-    })
-
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
